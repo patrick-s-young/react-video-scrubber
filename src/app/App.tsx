7624df27b05127601ca490d4ff5f7d8ab32d146e
Patrick S. Young <patrick@bigeyeball.co>
2020-10-23 08:43:10 -0700

    incorporate <VideoUpload> component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,35 @@
 import * as React from 'react';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
 import Scrubber from 'features/videoScrubber/Scrubber';
-import { videoSrc, scrubberFramesMax } from 'app/app.config';
-import 'app/App.css';
+import { VideoUpload } from 'features/userVideo/VideoUpload';
+import type { VideoState } from 'features/userVideo/videoSlice';
+import type { RootState } from 'app/rootReducer';
+import 'app/app.css';
 
-const App: React.FC = () =>
-	<div className='app-container'>
-		<div>
-			SOURCE
-		</div>
-		<div>
-			FRAME SCRUBBER
-		</div>
-		<div> 
-			<video autoPlay muted loop >
-				<source src={videoSrc} type='video/mp4' />  
-			</video>
-		</div>
-		<div>
-			<Scrubber
-        videoSrc={videoSrc}
-        scrubberFramesMax={scrubberFramesMax}
-			/>
-		</div>
-	</div>
-
-
+const App: React.FC = () => {
+	// conditional render <VideoUpload> or <MosaicTiles>
+	const [videoReady, setVideoReady] = useState<boolean>(false);
+	// set app screen width relative to video width
+	const { src, duration, width, height } = useSelector<RootState, VideoState>((state) => state.video as VideoState);
+	const appScreenWidth: Partial<number> = width > window.innerWidth ? window.innerWidth : width;
+	// callback from <VideoUpload> when user-selected video is ready
+	const onVideoSelectedCallback = () => {
+		setVideoReady(true);
+	}
+	// <VideoUpload> is a mock for user-initiated video uploads
+	return (
+			<div className='app-container' style={{width: `${appScreenWidth}px`}}>
+				{!videoReady
+					? <VideoUpload onVideoSelectedCallback={onVideoSelectedCallback}/>
+					: <Scrubber
+							src={src}
+							duration={duration}
+							width={width}
+							height={height}/>
+				}
+			</div>
+	);
+}
+			
 export default App;
